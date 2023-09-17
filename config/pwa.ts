f2d8ad62ea2dd3bc611b830169709e2ce49fcb7c
Kiwi2333 <1329634286@qq.com>
2023-09-17 09:51:29 +0800

    修改pwa设置

--- config/pwa.ts
+++ config/pwa.ts
@@ -1,61 +1,60 @@
-import type { ModuleOptions } from '@vite-pwa/nuxt'
-import { appDescription, appName } from '../constants/index'
+import type { ModuleOptions } from "@vite-pwa/nuxt";
+import { appDescription, appName } from "../constants/index";
 
-const scope = '/'
+const scope = "/";
 export const pwa: ModuleOptions = {
-  registerType: 'autoUpdate',
+  registerType: "autoUpdate",
   scope,
   base: scope,
+  display: "standalone",
+  includeAssets: ["logo.png"],
   // https://developer.mozilla.org/zh-CN/docs/Web/Manifest#lang
   manifest: {
-    id: scope,
-    scope,
     background_color: "#fff",
     display: "standalone",
     name: appName,
     short_name: appName,
     start_url: scope,
     description: appDescription,
-    theme_color: '#5d33f6',
+    theme_color: "#5d33f6",
     icons: [
       {
-        src: '/logo.png',
-        sizes: 'any',
-        type: 'image/png',
-        purpose: 'maskable '
-      }
-      ,
-      { "purpose": "any", "sizes": "any", "src": "glowing_star.svg", "type": "image/svg+xml" }
+        src: "/logo.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable ",
+      },
+      { purpose: "any", sizes: "192x192", src: "glowing_star.svg", type: "image/svg+xml" },
     ],
-    related_applications: [
-      // {
-      //   platform: "play",
-      //   url: "https://play.google.com/store/apps/details?id=cheeaun.hackerweb"
-      // }
-    ]
+    // related_applications: [
+    // {
+    //   platform: "play",
+    //   url: "https://play.google.com/store/apps/details?id=cheeaun.hackerweb"
+    // }
+    // ],
   },
   workbox: {
-    globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,ttf,woff,woff2}'],
+    globPatterns: ["**/*.{css,js,html,svg,png,ico,txt,ttf,woff,woff2}"],
     runtimeCaching: [
       {
-        urlPattern: "/^https://fonts.googleapis.com/.* /i",
-        handler: 'CacheFirst',
+        urlPattern: "/^https://quang.kiwi2333.top/font/* /i",
+        handler: "CacheFirst",
         options: {
-          cacheName: 'google-fonts-cache',
+          cacheName: "jiwuquan-fonts-cache",
           expiration: {
             maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+            maxAgeSeconds: 60 * 60 * 24 * 31, // <== 365 days
           },
           cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
+            statuses: [0, 200],
+          },
+        },
       },
-    ]
+    ],
   },
   registerWebManifestInRouteRules: true,
   writePlugin: true,
   devOptions: {
     enabled: true,
   },
-}
+};
